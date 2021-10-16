a8e5a1c3a4a5779e841d27811903601961a7b086
Peter Stuart <peter@peterstuart.org>
2021-10-16 07:06:11 -0400

    LED roulette

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -2,14 +2,41 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use aux5::{entry, Delay, DelayMs, LedArray, OutputSwitch};
+
+const CYCLE_PERIOD_MS: u16 = 250;
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let tick_delay: u16 = CYCLE_PERIOD_MS / (leds.len() as u16);
+    let cycle: usize = leds.len() * 2;
+
+    let mut tick: usize = 0;
+
+    loop {
+        turn_on(&mut leds, tick);
+        turn_off(&mut leds, tick);
+        delay.delay_ms(tick_delay);
+        tick = (tick + 1) % cycle;
+    }
+}
+
+fn turn_on(leds: &mut LedArray, tick: usize) {
+    if tick % 2 != 0 {
+        return;
+    }
+
+    let index = (tick / 2 + 1) % leds.len();
+    leds[index].on().unwrap();
+}
+
+fn turn_off(leds: &mut LedArray, tick: usize) {
+    if tick % 2 != 1 {
+        return;
+    }
+
+    let index = tick / 2;
+    leds[index].off().unwrap();
 }
