46ecc4ddfdb7849f418e807da217d5c271d38c54
Peter Stuart <peter@peterstuart.org>
2021-10-17 13:22:09 -0400

    uprintln!

--- src/11-usart/src/main.rs
+++ src/11-usart/src/main.rs
@@ -1,30 +1,50 @@
+#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
+use core::fmt::{self, Write};
+
 #[allow(unused_imports)]
-use aux11::{entry, iprint, iprintln};
+use aux11::{entry, iprint, iprintln, usart1};
+
+macro_rules! uprint {
+    ($serial:expr, $($arg:tt)*) => {
+        $serial.write_fmt(format_args!($($arg)*)).ok()
+    };
+}
+
+macro_rules! uprintln {
+    ($serial:expr, $fmt:expr) => {
+        uprint!($serial, concat!($fmt, "\n"))
+    };
+    ($serial:expr, $fmt:expr, $($arg:tt)*) => {
+        uprint!($serial, concat!($fmt, "\n"), $($arg)*)
+    };
+}
+
+struct SerialPort {
+    usart1: &'static mut usart1::RegisterBlock,
+}
+
+impl fmt::Write for SerialPort {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for byte in s.bytes() {
+            while self.usart1.isr.read().txe().bit_is_clear() {}
+
+            self.usart1.tdr.write(|w| w.tdr().bits(u16::from(byte)));
+        }
+
+        Ok(())
+    }
+}
 
 #[entry]
 fn main() -> ! {
-    let (usart1, mono_timer, mut itm) = aux11::init();
+    let (usart1, _mono_timer, _itm) = aux11::init();
 
-    let instant = mono_timer.now();
+    let mut serial = SerialPort { usart1 };
 
-    for byte in "The quick brown fox jumps over the lazy dog.".bytes() {
-        while usart1.isr.read().txe().bit_is_clear() {}
+    uprintln!(serial, "The answer is {}", 40 + 2);
 
-        usart1.tdr.write(|w| w.tdr().bits(u16::from(byte)));
-    }
-
-    let elapsed = instant.elapsed();
-
-    iprintln!(
-        &mut itm.stim[0],
-        "`for` loop took {} ticks ({} us)",
-        elapsed,
-        elapsed as f32 / mono_timer.frequency().0 as f32 * 1e6
-    );
-
-    // testing
     loop {}
 }
