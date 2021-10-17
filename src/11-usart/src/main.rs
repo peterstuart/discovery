8c4ce15f8e0a406cacc56ff4e49f5d6050f5d2f5
Peter Stuart <peter@peterstuart.org>
2021-10-17 15:55:15 -0400

    Echo server

--- src/11-usart/src/main.rs
+++ src/11-usart/src/main.rs
@@ -2,49 +2,19 @@
 #![no_main]
 #![no_std]
 
-use core::fmt::{self, Write};
-
 #[allow(unused_imports)]
-use aux11::{entry, iprint, iprintln, usart1};
-
-macro_rules! uprint {
-    ($serial:expr, $($arg:tt)*) => {
-        $serial.write_fmt(format_args!($($arg)*)).ok()
-    };
-}
-
-macro_rules! uprintln {
-    ($serial:expr, $fmt:expr) => {
-        uprint!($serial, concat!($fmt, "\n"))
-    };
-    ($serial:expr, $fmt:expr, $($arg:tt)*) => {
-        uprint!($serial, concat!($fmt, "\n"), $($arg)*)
-    };
-}
-
-struct SerialPort {
-    usart1: &'static mut usart1::RegisterBlock,
-}
-
-impl fmt::Write for SerialPort {
-    fn write_str(&mut self, s: &str) -> fmt::Result {
-        for byte in s.bytes() {
-            while self.usart1.isr.read().txe().bit_is_clear() {}
-
-            self.usart1.tdr.write(|w| w.tdr().bits(u16::from(byte)));
-        }
-
-        Ok(())
-    }
-}
+use aux11::{entry, iprint, iprintln};
 
 #[entry]
 fn main() -> ! {
     let (usart1, _mono_timer, _itm) = aux11::init();
 
-    let mut serial = SerialPort { usart1 };
+    loop {
+        // Wait until there's data available
+        while usart1.isr.read().rxne().bit_is_clear() {}
 
-    uprintln!(serial, "The answer is {}", 40 + 2);
-
-    loop {}
+        // Retrieve the data
+        let byte = usart1.rdr.read().rdr().bits();
+        usart1.tdr.write(|w| w.tdr().bits(byte));
+    }
 }
