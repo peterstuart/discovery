6a3cacb59c7235e51dc0f35233880678327fa543
Peter Stuart <peter@peterstuart.org>
2021-10-17 13:18:51 -0400

    USART 1

--- src/11-usart/src/main.rs
+++ src/11-usart/src/main.rs
@@ -6,12 +6,25 @@ use aux11::{entry, iprint, iprintln};
 
 #[entry]
 fn main() -> ! {
-    let (usart1, _mono_timer, _itm) = aux11::init();
+    let (usart1, mono_timer, mut itm) = aux11::init();
 
-    // Send a single character
-    usart1
-        .tdr
-        .write(|w| w.tdr().bits(u16::from(b'X')) );
+    let instant = mono_timer.now();
 
+    for byte in "The quick brown fox jumps over the lazy dog.".bytes() {
+        while usart1.isr.read().txe().bit_is_clear() {}
+
+        usart1.tdr.write(|w| w.tdr().bits(u16::from(byte)));
+    }
+
+    let elapsed = instant.elapsed();
+
+    iprintln!(
+        &mut itm.stim[0],
+        "`for` loop took {} ticks ({} us)",
+        elapsed,
+        elapsed as f32 / mono_timer.frequency().0 as f32 * 1e6
+    );
+
+    // testing
     loop {}
 }
