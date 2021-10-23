bf61c80ebc9d1c2ca9039ed1a42488c25ad082ba
Peter Stuart <peter@peterstuart.org>
2021-10-23 08:59:10 -0400

    LED compass: solution 2

--- src/15-led-compass/src/main.rs
+++ src/15-led-compass/src/main.rs
@@ -2,8 +2,13 @@
 #![no_main]
 #![no_std]
 
+// You'll find this useful ;-)
+use core::f32::consts::{FRAC_PI_8, PI};
+
 #[allow(unused_imports)]
 use aux15::{entry, iprint, iprintln, prelude::*, switch_hal::OutputSwitch, Direction, I16x3};
+// this trait provides the `atan2` method
+use m::Float;
 
 #[entry]
 fn main() -> ! {
@@ -13,22 +18,29 @@ fn main() -> ! {
     loop {
         let I16x3 { x, y, .. } = lsm303dlhc.mag().unwrap();
 
-        // Look at the signs of the X and Y components to determine in which
-        // quadrant the magnetic field is
-        let dir = match (x > 0, y > 0) {
-            // Quadrant ???
-            (true, true) => Direction::Southeast,
-            // Quadrant ???
-            (false, true) => Direction::Northeast,
-            // Quadrant ???
-            (false, false) => Direction::Northwest,
-            // Quadrant ???
-            (true, false) => Direction::Southwest,
+        let theta = (y as f32).atan2(x as f32); // in radians
+
+        let dir = if theta >= FRAC_PI_8 && theta <= 3.0 * FRAC_PI_8 {
+            Direction::Southeast
+        } else if theta > 3.0 * FRAC_PI_8 && theta <= 5.0 * FRAC_PI_8 {
+            Direction::East
+        } else if theta > 5.0 * FRAC_PI_8 && theta <= 7.0 * FRAC_PI_8 {
+            Direction::Northeast
+        } else if theta > 7.0 * FRAC_PI_8 || theta <= -7.0 * FRAC_PI_8 {
+            Direction::North
+        } else if theta > -7.0 * FRAC_PI_8 && theta <= -5.0 * FRAC_PI_8 {
+            Direction::Northwest
+        } else if theta > -5.0 * FRAC_PI_8 && theta <= -3.0 * FRAC_PI_8 {
+            Direction::West
+        } else if theta > -3.0 * FRAC_PI_8 && theta <= -1.0 * FRAC_PI_8 {
+            Direction::Southwest
+        } else {
+            Direction::South
         };
 
         leds.iter_mut().for_each(|led| led.off().unwrap());
         leds[dir as usize].on().unwrap();
 
-        delay.delay_ms(1_000_u16);
+        delay.delay_ms(100_u8);
     }
 }
